54467f0531284d7765c591bc8938c6f66631072b
MeilyD07 <meilyd07@gmail.com>
2020-04-10 00:50:39 +0300

    feat: add date formatter test

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,28 @@
 package subtask1
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.ResolverStyle
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val inputString = "$day.$month.$year"
+        var strToDateFormatter = DateTimeFormatter.ofPattern("d.M.uuuu").withResolverStyle(
+            ResolverStyle.STRICT)
+
+        return try {
+            var date = LocalDate.parse(inputString, strToDateFormatter)
+            var toStringFormatter = DateTimeFormatter.ofPattern("d MMMM, EEEE", Locale("ru"))
+            var formattedDate = date.format(toStringFormatter)
+            formattedDate
+        } catch (e: Exception) {
+            "Такого дня не существует";
+        }
+
+
     }
 }
