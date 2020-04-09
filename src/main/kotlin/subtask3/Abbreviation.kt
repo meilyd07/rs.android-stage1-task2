806370300e3fd926b3cf6b7db7c1e22756f10929
MeilyD07 <meilyd07@gmail.com>
2020-04-09 22:26:52 +0300

    feat: add abbreviation test and block test

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,16 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val uppercaseA = a.toUpperCase()
+        val bSet = b.toMutableList()
+        val aSet = uppercaseA.toMutableList()
+
+        val intersectedA = aSet.intersect(bSet).toMutableList()
+
+        return if (intersectedA == bSet) {
+            "YES"
+        } else {
+            "NO"
+        }
     }
 }
--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,61 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
-    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+    fun getData(blockA: Array<*>, blockB: KClass<*>): Any? {
+
+        return when (blockB) {
+            Int :: class -> {
+
+                var totalSum = 0
+                for(item in blockA) {
+                    if (item is Int) {
+                        totalSum += item.toInt()
+                    }
+                }
+                totalSum
+
+            }
+            String :: class -> {
+
+                var concatString: StringBuilder = StringBuilder()
+                for (item in blockA) {
+                    if (item is String) {
+                        concatString.append(item)
+                    }
+                }
+                concatString.toString()
+            }
+            LocalDate :: class -> {
+
+                var dateIndex = -1
+                for ((i, value) in blockA.withIndex()) {
+                    if (value is LocalDate) {
+                        if (dateIndex < 0) {
+                            dateIndex = i
+                        } else {
+                            if (value > (blockA[i] as LocalDate)) {
+                                dateIndex = i
+                            }
+                        }
+                    }
+                }
+
+                if (dateIndex > 0) {
+                    var formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+                    (blockA[dateIndex] as LocalDate).format(formatter)
+                } else {
+                    null
+                }
+            }
+        else -> {
+                null
+            }
+        }
     }
 }
